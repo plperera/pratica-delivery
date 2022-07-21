7d2dd7ca05e2f7a96bfda5f473a1384b53057f56
Pedro Pereira <plp.leoncio@gmail.com>
2022-07-21 17:24:13 -0300

    mudança nos arquivos base

--- src/Menu.js
+++ src/Menu.js
@@ -7,6 +7,7 @@ export default function Menu() {
     <img src="./assets/img/logo.jpg" alt=""></img>
     <span className="tittle">Lanche</span>
     <div className="button">Iniciar Recall!</div>
+    <h1>jiodfwd</h1>
 
     </div>
     )
